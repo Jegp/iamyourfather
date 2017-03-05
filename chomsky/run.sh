6bda2ff40695e0f1b52c6e55c675b71b3d89496c
Sotiris Lamprinidis <sot.lampr@gmail.com>
2017-03-05 20:32:06 +0100

    A basic script (works on server)

---
+++ chomsky/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+function exit_if_error {
+	# Check the exit status of the previous function,
+	# if it is not zero then just exit.
+	if [$? -ne 0 ]
+	then
+		echo "An error has occured, exiting..."
+	fi
+}
+
+export ESTDIR=/usr
+export PATH=$PATH:.
+
+mkdir -p {bin,dic,festvox,group,lpc,pm,wav}
+
+echo "Performing pitch analysis..."
+./bin/make_pm_wave wav/*.wav
+exit_if_error()
+
+echo "Contents of pm/"
+ls -lsh pm/
+
+echo "Performing LPC analysis..."
+./bin/make_lpc wav/*.wav
+exit_if_error()
+
+echo "Contents of pm/"
+ls -lsh lpc/
