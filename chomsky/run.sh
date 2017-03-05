8fafa2ad8a3883338d04bfc30ed6fd207cc3e10e
Sotiris Lamprinidis <sot.lampr@gmail.com>
2017-03-05 20:42:25 +0100

    Bug fixes in run.sh

--- chomsky/run.sh
+++ chomsky/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 function exit_if_error {
 	# Check the exit status of the previous function,
 	# if it is not zero then just exit.
@@ -15,14 +15,20 @@ mkdir -p {bin,dic,festvox,group,lpc,pm,wav}
 
 echo "Performing pitch analysis..."
 ./bin/make_pm_wave wav/*.wav
-exit_if_error()
+exit_if_error
 
 echo "Contents of pm/"
 ls -lsh pm/
 
 echo "Performing LPC analysis..."
 ./bin/make_lpc wav/*.wav
-exit_if_error()
+exit_if_error
 
 echo "Contents of pm/"
 ls -lsh lpc/
+
+echo "Compiling TTS (group files)"
+./make_grp
+exit_if_error
+
+echo "Done!"
